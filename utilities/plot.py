9397fbb68ce4eac4a069298de129ed42e430bf01
Atreju Tauschinsky <Atreju.Tauschinsky@gmx.de>
2014-09-16 23:58:57 +0200

    plot.py: minor changes

--- utilities/plot.py
+++ utilities/plot.py
@@ -3,12 +3,7 @@ from matplotlib import pyplot as plt
 from scipy.interpolate import RectBivariateSpline
 import os
 
-folder = 'mm/B-scan/'
-
-A = np.genfromtxt('../results/' + folder + '/B  0.00.txt').T
-
-plt.figure()
-plt.imshow(A)
+folder = 'pp/'
 
 dp = -20+0.5*np.arange(81)
 dc = -50+0.5*np.arange(201)
@@ -16,21 +11,10 @@ Delta = np.linspace(-50, 40, 100)
 vvals = np.linspace(-15, 15, 1000)
 signal = np.zeros_like(Delta)
 
-spline = RectBivariateSpline(dp, dc, A)
-
-for i, D in enumerate(Delta):
-	vres = spline.ev(vvals/780.e-3, D-vvals/486.e-3)*np.exp(-(vvals/240.)**2)
-	signal[i] = np.exp(np.trapz(vres, vvals))
-
-plt.figure()
-plt.plot(Delta, signal)
-
-plt.show()
-
 # now here we do it for a whole scan of B-field values
 files = [x for x in os.listdir('../results/' + folder) if x.endswith('.txt')]
-scanres = np.zeros((100, 31))
-scanres0 = np.zeros((100, 31))
+scanres = np.zeros((100, len(files)))
+scanres0 = np.zeros((100, len(files)))
 for f in files:
 	A = np.genfromtxt('../results/' + folder + f).T
 	index = float(f[1:7]) + 15
