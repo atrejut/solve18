632f59ff47afa894a48dede709cbbd56b1ec1cfc
junaber@LISA <j.b.naber@uva.nl>
2014-09-19 10:47:24 +0200

    improvements to lisa autoscanner

--- utilities/autoscan_lisa.py
+++ utilities/autoscan_lisa.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python
 
 import numpy as np
-import subprocess
+import subprocess, os
+from datetime import datetime
 
-bvals = np.arange(-15, 15, 1)
+bvals = np.linspace(-15, 15, 151)
+pol = 'mm'
+name = 'testrun_mm'
+
+timestamp = datetime.now().strftime('%Y-%m-%d/%H%M%S')
+
+outfolder = os.environ['HOME'] + "/atreju/results/" + timestamp + '-' + name
+
+if not os.path.exists(outfolder):
+  os.makedirs(outfolder)
 
 for b in bvals:
   # write job script
-  with open('../tmp/jobscript', 'wb') as ifile:
-    ifile.write('#PBS -lwalltime=25:00\n')
-    ifile.write('#PBS -lnodes=1\n')
+  with open('../tmp/job-' + name, 'wb') as ifile:
+    ifile.write('#PBS -lwalltime=10:00\n')
+    ifile.write('#PBS -lnodes=1:cpu3\n')
     ifile.write('cd $TMPDIR\n')
-    ifile.write('cp $HOME/atreju/solve18/bin/solve18_pp "$TMPDIR"\n')
+    ifile.write('cp $HOME/atreju/solve18/bin/solve18_%s "$TMPDIR"\n' %pol)
     # write new config.info
     ifile.write('echo "&PARAMS" > config.info\n')
     ifile.write('echo "bfield = %14.12fd0," >> config.info\n' %b)
-    ifile.write('echo "Wc1 = 1.d0," >> config.info\n')
-    ifile.write('echo "Wc2 = 1.d0," >> config.info\n')
+    ifile.write('echo "Wc1 = 2.d0," >> config.info\n')
+    ifile.write('echo "Wc2 = 2.d0," >> config.info\n')
     ifile.write('echo "Wp = 0.1d0," >> config.info\n')
     ifile.write('echo "hfs = 8.d0," >> config.info\n')
     ifile.write('echo "G5p = 6.d0," >> config.info\n')
@@ -26,21 +36,22 @@ for b in bvals:
     ifile.write('echo "gc = 0.8d0" >> config.info\n')
     ifile.write('echo "/" >> config.info\n')
     ifile.write('echo "&STEPPING" >> config.info\n')
-    ifile.write('echo "NProbe = 81," >> config.info\n')
-    ifile.write('echo "NCoupling = 201," >> config.info\n')
+    ifile.write('echo "NProbe = 161," >> config.info\n')
+    ifile.write('echo "NCoupling = 801," >> config.info\n')
     ifile.write('echo "ShiftProbe = -20," >> config.info\n')
-    ifile.write('echo "ShiftCoupling = -50," >> config.info\n')
-    ifile.write('echo "StepProbe = 0.5d0," >> config.info\n')
-    ifile.write('echo "StepCoupling = 0.5d0" >> config.info\n')
+    ifile.write('echo "ShiftCoupling = -100," >> config.info\n')
+    ifile.write('echo "StepProbe = 0.25d0," >> config.info\n')
+    ifile.write('echo "StepCoupling = 0.25d0" >> config.info\n')
     ifile.write('echo "/" >> config.info\n')
     ifile.write('echo "&SIM" >> config.info\n')
-    ifile.write('echo "pol = \\\"pp\\\"" >> config.info\n')
+    ifile.write('echo "pol = \\\"%s\\\"" >> config.info\n' %pol)
     ifile.write('echo "/" >> config.info\n')
     ifile.write('cat config.info\n')
     # run program
-    ifile.write('./solve18_pp\n')
+    ifile.write('FORT_FMT_RECL=100000 ./solve18_%s\n'%pol)
     # return results to home drive
-    ifile.write('cp output.txt $HOME/atreju/bscan/result' + str(b) + '\n')
+    ifile.write('cp output.txt "%s/data%5.2f"\n' %(outfolder, b))
   
-  subprocess.call(['qsub',  'jobscript'], cwd='../tmp/')
+  while subprocess.call(['qsub',  'job-' + name], cwd='../tmp/') != 0:
+    print 'qsub timed out, trying again with identical parameters'
 
